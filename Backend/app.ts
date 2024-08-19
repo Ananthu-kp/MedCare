c06a835dabb881be2a4deb8fcc1bb8dee7c4e6f0
Ananthakrishnan KP <ananthukp17@gmail.com>
2024-08-19 23:58:59 +0530

    initial commit

--- Backend/app.ts
+++ Backend/app.ts
@@ -1,14 +1,23 @@
 import express from "express";
 import dotenv from "dotenv"
+import './src/config/cronJob/cronJobs'
 dotenv.config()
 const app = express();
 import cors from "cors"
+import userRoutes from './src/routes/userRoute'
 import dbConnection from "./src/config/dbConnect";
 dbConnection();
 
 
+
 app.use(express.json());
-app.use(cors())
+app.use(cors({
+    origin: 'http://localhost:5173',
+    methods:    'GET, POST, PUT, DELETE',
+    credentials: true
+}))
+
+app.use('/', userRoutes)
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => console.log(`Server is Running on: http://localhost:${PORT}`))
