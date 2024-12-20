dd95254042c8dda6350ec82a8b93c4f2fe9a15f5
Ananthakrishnan KP <ananthukp17@gmail.com>
2024-12-20 23:46:27 +0530

    nothing

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -25,7 +25,7 @@ app.use(cors({
     origin: 'http://localhost:5173',
     methods:    'GET, POST, PUT, DELETE, PATCH',
     credentials: true
-}))
+}));
 
 app.use('/', userRoutes)
 app.use('/admin', adminRoutes)
